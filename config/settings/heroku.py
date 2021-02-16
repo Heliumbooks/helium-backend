148b21ef12f6c01f0568918bd367cd496dd452bd
charles clark <charles@pop-os.localdomain>
2021-02-15 20:47:32 -0600

    Updated heroku file

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -11,3 +11,8 @@ MIDDLEWARE = MIDDLEWARE + ['whitenoise.middleware.WhiteNoiseMiddleware']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ALLOWED_HOSTS = ['*']
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
