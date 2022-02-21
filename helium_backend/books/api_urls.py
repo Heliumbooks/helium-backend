787211fe78c04b8ce8dc52e2b7ae98cfcfe63c44
Charles Clark <charles.t.clark89@gmail.com>
2022-02-21 17:06:30 -0600

    removing url reference

--- helium_backend/books/api_urls.py
+++ helium_backend/books/api_urls.py
@@ -1,9 +1,9 @@
 from django.urls import path 
 
-from helium_backend.books.api import DatabaseLoad, DatabaseUpdate, UpdateBookInfo
+from helium_backend.books.api import DatabaseUpdate
 
 urlpatterns = [
-    path('seed/', DatabaseLoad.as_view()),
+    # path('seed/', DatabaseLoad.as_view()),
     path('update/', DatabaseUpdate.as_view()),
-    path('book-info-update/', UpdateBookInfo.as_view())
+    # path('book-info-update/', UpdateBookInfo.as_view())
 ]
