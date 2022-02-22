18047497b58a858e85ac6f12be6d5a810002f722
Charles Clark <charles.t.clark89@gmail.com>
2022-02-21 18:08:19 -0600

    moved processing of open library updates to separate function

--- helium_backend/books/api.py
+++ helium_backend/books/api.py
@@ -1,4 +1,5 @@
 from os import stat
+from py import process
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -10,6 +11,63 @@ from datetime import datetime
 import pytz
 
 from helium_backend.books.models import Book, Author, Subject, IsbnNumber
+
+
+def process_book_updates(books):
+    api_base_url = "https://openlibrary.org/search.json?"
+    count = 0
+    for item in books:
+        print(item.get('title'))
+        formatted_author = item.get('author__full_name').replace(" ", "+")
+        formatted_title = item.get('title').replace(" ", "+")
+        try:
+            if count % 90 == 0 and count != 0:
+                time.sleep(300)
+            response = requests.get(f"{api_base_url}author={formatted_author}&title={formatted_title}").json()
+            count += 1
+            if response.get('numFound') > 0:
+                book = Book.objects.filter(pk=item.get('id')).first()
+                book_ol_id = response.get('docs')[0].get('key').split("/")[-1]
+                book.book_ol_id = book_ol_id
+                pages = response.get('docs')[0].get('number_of_pages_median')
+                if pages:
+                    book.pages = pages
+                # Need to convert this to an acutal date
+                published_dates = response.get('docs')[0].get('publish_date')
+                for item in published_dates:
+                    if len(item) == 4:
+                        publish_date = datetime(int(item), 1, 1, 0, 0, 0, 0, tzinfo=pytz.UTC)
+                        book.published_date = publish_date
+                        break
+
+                if response.get('docs')[0].get('isbn'):
+                    for x in response.get('docs')[0].get('isbn'):
+                        isbn, isbn_created = IsbnNumber.objects.get_or_create(
+                            book=book,
+                            isbn=x
+                        )
+                if response.get('docs')[0].get('subject'):
+                    for i in response.get('docs')[0].get('subject'):
+                        subject = Subject.objects.filter(title=i).first()
+                        if subject:
+                            subject.books_in_subject += 1
+                            subject.save()
+                            book.subjects.add(subject)
+                        else:
+                            subject = Subject.objects.create(
+                                title=i,
+                                books_in_subject=1
+                            )
+                            book.subjects.add(subject)
+                book.processed_from_open_lib = True
+                book.save()
+                print(f"Successfully processed {book.title}")
+                continue
+            else:
+                print('book not in database')
+        except Exception as e:
+            print('unable to get books')
+            print(e)
 
 
 # class DatabaseLoad(APIView):
@@ -39,59 +97,9 @@ class DatabaseUpdate(APIView):
     def post(self, request):
         api_base_url = "https://openlibrary.org/search.json?"
         books = Book.objects.filter(processed_from_open_lib=False).values('id', 'title', 'author__full_name').order_by('title')
-        count = 0
-        for item in books:
-            formatted_author = item.get('author__full_name').replace(" ", "+")
-            formatted_title = item.get('title').replace(" ", "+")
-            try:
-                if count % 90 == 0 and count != 0:
-                    time.sleep(300)
-                response = requests.get(f"{api_base_url}author={formatted_author}&title={formatted_title}").json()
-                count += 1
-                if response.get('numFound') > 0:
-                    book = Book.objects.filter(pk=item.get('id')).first()
-                    book_ol_id = response.get('docs')[0].get('key').split("/")[-1]
-                    book.book_ol_id = book_ol_id
-                    pages = response.get('docs')[0].get('number_of_pages_median')
-                    if pages:
-                        book.pages = pages
-                    # Need to convert this to an acutal date
-                    published_dates = response.get('docs')[0].get('publish_date')
-                    for item in published_dates:
-                        if len(item) == 4:
-                            publish_date = datetime(int(item), 1, 1, 0, 0, 0, 0, tzinfo=pytz.UTC)
-                            book.published_date = publish_date
-                            break
+        process_book_updates(books)
 
-                    if response.get('docs')[0].get('isbn'):
-                        for x in response.get('docs')[0].get('isbn'):
-                            isbn, isbn_created = IsbnNumber.objects.get_or_create(
-                                book=book,
-                                isbn=x
-                            )
-                    if response.get('docs')[0].get('subject'):
-                        for i in response.get('docs')[0].get('subject'):
-                            subject = Subject.objects.filter(title=i).first()
-                            if subject:
-                                subject.books_in_subject += 1
-                                subject.save()
-                                book.subjects.add(subject)
-                            else:
-                                subject = Subject.objects.create(
-                                    title=i,
-                                    books_in_subject=1
-                                )
-                                book.subjects.add(subject)
-                    book.processed_from_open_lib = True
-                    book.save()
-                    continue
-                else:
-                    print('book not in database')
-            except Exception as e:
-                print('unable to get books')
-                print(e)
-
-        data = {"status": "complete"}
+        data = {"status": "Function has started successfully"}
         return Response(status=status.HTTP_200_OK, data=data)
 
 # class UpdateBookInfo(APIView):
